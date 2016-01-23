899c6a04dac7f37191fd68c62b16db6b75c15a56
SANDS <DYLAN-SANDS@umflint.edu>
2016-01-23 13:37:21 -0500

    changed variables in robotmap and added chassis to them

--- 2016/src/org/usfirst/frc/team4998/robot/RobotMap.java
+++ 2016/src/org/usfirst/frc/team4998/robot/RobotMap.java
@@ -18,10 +18,8 @@ public class RobotMap {
 	public static int button0 = 0;
 	public static int button1 = 1;
 
-	public static int frontLeft = 0;
-	public static int frontRight = 1;
-	public static int rearLeft = 2;
-	public static int rearRight = 3;
+	public static int chassisLeft = 0;
+	public static int chassisRight = 1;
 	
 	public static int rightShooter = 1;
 	public static int leftShooter = 2;
