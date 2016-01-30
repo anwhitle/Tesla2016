70bf33243cfaa3da743933df2e72bfa1966fcace
BLUE <ANTWAN-BLUE@umflint.edu>
2016-01-30 12:06:40 -0500

    Added sonic sensor and will return values in inches, also added return values for gyro sensor.

--- 2016/src/org/usfirst/frc/team4998/robot/subsystems/Chassis.java
+++ 2016/src/org/usfirst/frc/team4998/robot/subsystems/Chassis.java
@@ -2,15 +2,18 @@
 package org.usfirst.frc.team4998.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.VictorSP;
 import org.usfirst.frc.team4998.robot.RobotMap;
 /**
  *
  */
-public class Chassis extends Subsystem {
+public class Chassis extends Subsystem { 
 	VictorSP left,right;
 	AnalogGyro gyro;
+	Ultrasonic sonicSensor;
+	public static int ultrasonic;
 	final int gyroChannel = 0; //analog input
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -20,9 +23,8 @@ public class Chassis extends Subsystem {
     	left   = new VictorSP (RobotMap.chassisLeft);
         right  = new VictorSP (RobotMap.chassisRight);
         gyro = new AnalogGyro(gyroChannel);
-
-        
-    
+    	ultrasonic = 1;
+    	sonicSensor = new Ultrasonic (1,2);
     }
     
     public void drive(double z, double y){
@@ -39,5 +41,11 @@ public class Chassis extends Subsystem {
 
     }
     
+    public double getRange()   {
+    	return sonicSensor.getRangeInches();
+    }
+    public double getAngle() {
+    	return gyro.getAngle();
+    }
 }
 
